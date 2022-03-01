324ccdd70bc4dc1ab31316907951bb13f69ff625
Dean Wallflower <dean@Deans-MacBook-Pro.fritz.box>
2022-03-01 10:09:54 +0100

    main js

---
+++ script.js
@@ -0,0 +1,16 @@
+const columnLetters = ['a', 'b', 'c']
+const playField = document.querySelector("#board")
+
+const columns = 3;
+const rows = 3;
+
+const position = `${gridChars[i]}-${j}`;
+
+for (let i = 0; i < columnLetters.length; i++) {
+    for (let j = 0; j < rows; j++) {
+        const gridCell = document.createElement('div');
+        gridCell.id = `${columnLetters[i]}-${j}`;
+        gridCell.classList.add('cell')
+        playField.appendChild(gridCell)
+    }
+}
