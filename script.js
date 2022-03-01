70aa88d737db9c53e98ea5e017d8c9ff6b8d7780
Tanous Atallah <tanous.atallah@gmail.com>
2022-03-01 11:46:07 +0100

    bla bla tony

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-const columnLetters = ["a", "b", "c"];
+const log = console.log;
+// const columnLetters = ["a", "b", "c"];
 const playField = document.querySelector("#board");
+const msg = document.getElementById("msg");
+const CELLS = 9;
 
-const columns = 3;
-const rows = 3;
 
-for (let i = 0; i < columnLetters.length; i++) {
-  for (let j = 0; j < rows; j++) {
+for (let i = 1; i <= CELLS; i++) {
+  // for (let j = 1; j < rows; j++) {
     const gridCell = document.createElement("div");
-    gridCell.id = `${columnLetters[i]}-${j}`;
+    gridCell.id = `c${i}`;
+    gridCell.setAttribute("data-empty","none");
     gridCell.classList.add("cell");
     playField.appendChild(gridCell);
+    gridCell.addEventListener("click",checkCell )
+  // }
+}
+
+log(playField)
+
+let isPlayerOneTurn = true; 
+
+function checkCell(e) {
+  if(e.target.dataset.empty == 'none'){
+
+    if(isPlayerOneTurn){
+      e.target.innerText = "X"
+      isPlayerOneTurn = false
+    }else{
+      e.target.innerText = "O"
+      isPlayerOneTurn= true
+    }
+    e.target.dataset.empty = 'set';
   }
+
+  
 }
