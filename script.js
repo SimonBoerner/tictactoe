bb91f8308a87eca0955657eb9b2b74f1f70ed67c
Tanous Atallah <tanous.atallah@gmail.com>
2022-03-01 13:02:24 +0100

    human vs human: project completed

--- script.js
+++ script.js
@@ -2,36 +2,124 @@ const log = console.log;
 // const columnLetters = ["a", "b", "c"];
 const playField = document.querySelector("#board");
 const msg = document.getElementById("msg");
+const btnReset = document.getElementById("reset");
 const CELLS = 9;
-
+const MSG1 = "Player One Turn";
+const MSG2 = "Player Two Turn";
+let count = 9;
 
 for (let i = 1; i <= CELLS; i++) {
   // for (let j = 1; j < rows; j++) {
-    const gridCell = document.createElement("div");
-    gridCell.id = `c${i}`;
-    gridCell.setAttribute("data-empty","none");
-    gridCell.classList.add("cell");
-    playField.appendChild(gridCell);
-    gridCell.addEventListener("click",checkCell )
+  const gridCell = document.createElement("div");
+  gridCell.id = `c${i}`;
+  gridCell.setAttribute("data-empty", "none");
+  gridCell.classList.add("cell");
+  // gridCell.textContent = count++;
+  playField.appendChild(gridCell);
+  gridCell.addEventListener("click", checkCell);
   // }
 }
 
-log(playField)
+let divs = document.querySelectorAll(".cell");
 
-let isPlayerOneTurn = true; 
+// log(playField)
 
+let isPlayerOneTurn = true;
+let haveWinner = false;
 function checkCell(e) {
-  if(e.target.dataset.empty == 'none'){
-
-    if(isPlayerOneTurn){
-      e.target.innerText = "X"
-      isPlayerOneTurn = false
-    }else{
-      e.target.innerText = "O"
-      isPlayerOneTurn= true
+  if (!haveWinner) {
+    count--;
+    if (e.target.dataset.empty == "none") {
+      if (isPlayerOneTurn) {
+        e.target.innerText = "X";
+        e.target.classList.add("x");
+        isPlayerOneTurn = false;
+        msg.innerText = MSG2;
+        if (checkWinner("X")) {
+          // X player
+          msg.innerText = "Player One is Winner";
+          haveWinner = true;
+        }
+      } else {
+        e.target.innerText = "O";
+        e.target.classList.add("o");
+        isPlayerOneTurn = true;
+        msg.innerText = MSG1;
+        if (checkWinner("O")) {
+          // O player
+          msg.innerText = "Player Two is Winner";
+          haveWinner = true;
+        }
+      }
+      e.target.dataset.empty = "set";
     }
-    e.target.dataset.empty = 'set';
+    log(count)
+    if( !haveWinner && count == 0){
+      msg.textContent = 'Nobody is winner'
+    }
   }
-
-  
 }
+
+function checkWinner(player) {
+  if (
+    divs[0].textContent == player &&
+    divs[1].textContent == player &&
+    divs[2].textContent == player
+  )
+    return true;
+  if (
+    divs[3].textContent == player &&
+    divs[4].textContent == player &&
+    divs[5].textContent == player
+  )
+    return true;
+  if (
+    divs[6].textContent == player &&
+    divs[7].textContent == player &&
+    divs[8].textContent == player
+  )
+    return true;
+
+  if (
+    divs[0].textContent == player &&
+    divs[3].textContent == player &&
+    divs[6].textContent == player
+  )
+    return true;
+  if (
+    divs[1].textContent == player &&
+    divs[4].textContent == player &&
+    divs[7].textContent == player
+  )
+    return true;
+  if (
+    divs[2].textContent == player &&
+    divs[5].textContent == player &&
+    divs[8].textContent == player
+  )
+    return true;
+
+  if (
+    divs[0].textContent == player &&
+    divs[4].textContent == player &&
+    divs[8].textContent == player
+  )
+    return true;
+  if (
+    divs[2].textContent == player &&
+    divs[4].textContent == player &&
+    divs[6].textContent == player
+  )
+    return true;
+}
+
+btnReset.addEventListener("click", () => {
+  isPlayerOneTurn = true;
+  haveWinner = false;
+  for (let i = 0; i <= 8; i++) {
+    divs[i].dataset.empty = "none";
+    divs[i].classList.remove("x", "o");
+    divs[i].textContent = "";
+    msg.textContent = MSG1;
+  }
+});
