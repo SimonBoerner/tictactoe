198912c43bf56e8d3d79f3f0e55abbf8eaf2b5ea
SimonBoerner <boernersimon@gmail.com>
2022-03-01 10:55:57 +0100

    grid working

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const columnLetters = ['a', 'b', 'c']
-const playField = document.querySelector("#board")
+const columnLetters = ["a", "b", "c"];
+const playField = document.querySelector("#board");
 
 const columns = 3;
 const rows = 3;
 
-const position = `${gridChars[i]}-${j}`;
-
 for (let i = 0; i < columnLetters.length; i++) {
-    for (let j = 0; j < rows; j++) {
-        const gridCell = document.createElement('div');
-        gridCell.id = `${columnLetters[i]}-${j}`;
-        gridCell.classList.add('cell')
-        playField.appendChild(gridCell)
-    }
+  for (let j = 0; j < rows; j++) {
+    const gridCell = document.createElement("div");
+    gridCell.id = `${columnLetters[i]}-${j}`;
+    gridCell.classList.add("cell");
+    playField.appendChild(gridCell);
+  }
 }
